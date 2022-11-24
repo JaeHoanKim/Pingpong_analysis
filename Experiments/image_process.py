44f698a776524f2fb11218b370a2809c900c8cec
Jaehoan Kim <k1mjh6561@tamu.edu>
2022-11-24 15:04:24 +0900

    Rename the file

--- Experiments/image_process.py
+++ Experiments/image_process.py
@@ -47,18 +47,19 @@ for i in [frame_start, frame_end]:
     ret, fr = cap.read()
     frames_border.append(fr)
 plt.close();plt.figure()
-plt.subplot(211);plt.imshow(frames_border[0])
-plt.subplot(212);plt.imshow(frames_border[1])
-
-##################################
-#%% 4. Set the window manually ###
-##################################
+#plt.imshow(cv2.cvtColor(frame_with_ctr, cv2.COLOR_BGR2RGB))
+plt.subplot(211);plt.imshow(cv2.cvtColor(frames_border[0], cv2.COLOR_BGR2RGB))
+#plt.imshow(frames_border[0])
+plt.subplot(212);plt.imshow(cv2.cvtColor(frames_border[1], cv2.COLOR_BGR2RGB))
+###############################
+#%% Set the window manually ###
+###############################
 
 xmin, xmax = 650, 1150 # left and right end of the pingpong ball
 ymin, ymax = 0, 800 # bottom and top end of the pingpong ball
 
 ###############################################################
-#%% 5.calculate the variance of each pixel among the frames ###
+#%% 4.calculate the variance of each pixel among the frames ###
 ###############################################################
 # Intuition: the background is fixed and the motion of the pingpong ball generates the variance of the pixel
 # check_int : the matrix with the values of the variance of pixels
@@ -72,11 +73,11 @@ for i in range(frame_start, frame_end):
 frames_array = np.asarray(frames)
 check = np.std(frames_array, axis = 0)
 check_int = check.astype('uint8')
-plt.imshow(check_int)
+plt.imshow(cv2.cvtColor(check_int, cv2.COLOR_BGR2RGB))
 
-################################
-#%% 6. Convert to gray image ###
-################################
+#############################
+#%% Convert to gray image ###
+#############################
 
 ball_img = check_int[:,:,:]
 ball_gray = LA.norm(ball_img, axis = 2)
@@ -85,10 +86,10 @@ ball_gray = ball_gray.astype('uint8')
 plt.imshow(ball_gray, cmap='gray')
 
 #######################################################
-#%% 7. thresholding 1 - to clarify trajectory range ###
+#%% 5. thresholding 1 - to clarify trajectory range ###
 #######################################################
 
-thres = 20
+thres = 25
 index = np.where(ball_gray>=thres)
 check_path = np.zeros(ball_gray.shape)
 for  i in range(index[0].shape[0]):
@@ -96,7 +97,7 @@ for  i in range(index[0].shape[0]):
 plt.imshow(check_path, cmap = 'gray')
 
 ############################################################################
-#%% 8. thresholding 2 - to extract ball location using color information ###
+#%% 6. thresholding 2 - to extract ball location using color information ###
 ############################################################################
 
 bgr_orange = [0, 127, 255]
@@ -115,15 +116,15 @@ for  i in range(index2[0].shape[0]):
 ball_thres = ball_thres.astype('uint8')
 plt.imshow(ball_thres, cmap = 'gray')
 
-#######################################################
-#%% 9. Compare the loaction with the original image ###
-#######################################################
+####################################################
+#%% Compare the loaction with the original image ###
+####################################################
 
 plt.imshow(cv2.cvtColor(frame_test, cv2.COLOR_BGR2RGB))
 
-######################################################################
-#%% 10. find contour and extract the exact center of pingpong ball ###
-######################################################################
+#####################################################################
+#%% 7. find contour and extract the exact center of pingpong ball ###
+#####################################################################
 
 contours, _ = cv2.findContours(
     ball_thres, 
@@ -131,13 +132,15 @@ contours, _ = cv2.findContours(
     method=cv2.CHAIN_APPROX_SIMPLE
 )
 frame_with_ctr = frame_test.copy()
-cv2.drawContours(frame_with_ctr, contours=contours, contourIdx=-1, color=(255, 0, 255))
+cv2.drawContours(frame_with_ctr, contours=contours, contourIdx=-1, color=(255, 0, 255), thickness = 5)
 
 plt.imshow(cv2.cvtColor(frame_with_ctr, cv2.COLOR_BGR2RGB))
 # cv2.imshow('img', frame_with_ctr)
 cv2.waitKey(0)
 
-#%%
+########################################################
+#%% Making rectangular contour on the previous image ###
+########################################################
 contours_dict = []
 for contour in contours:
     x, y, w, h = cv2.boundingRect(contour)
@@ -152,18 +155,23 @@ for contour in contours:
         'cx': x + (w / 2),
         'cy': y + (h / 2)
     })
-plt.imshow(frame_with_ctr)
+plt.imshow(cv2.cvtColor(frame_with_ctr, cv2.COLOR_BGR2RGB))
 cv2.waitKey(0)
-#%%
-MIN_WIDTH, MIN_HEIGHT = 18, 18
+
+########################################################################################
+#%% Extracting the center of the pingpong ball using the restriction on the contours ###
+########################################################################################
+
+MIN_WIDTH, MIN_HEIGHT = 20, 20
 MAX_WIDTH, MAX_HEIGHT = 60, 60
-MIN_RATIO, MAX_RATIO = 0.5, 2.0
+MIN_RATIO, MAX_RATIO = 0.7, 1.3
 possible_contours = []
 cnt = 0
 for d in contours_dict:
     area = d['w'] * d['h']
     ratio = d['w'] / d['h']
     
+    # selecting the contour using contour size and width height ratio
     if d['w'] > MIN_WIDTH and d['h'] > MIN_HEIGHT \
     and d['w'] < MAX_WIDTH and d['h'] < MAX_HEIGHT \
     and MIN_RATIO < ratio < MAX_RATIO:
@@ -172,8 +180,16 @@ for d in contours_dict:
         possible_contours.append(d)
 if cnt >= 2 or cnt ==0:
     print('multiple or no contour selected')
+    
+######################################################################################################
+#%% 7. If multiple contours are selected, choose the exact contour manually (usually not required) ###
+######################################################################################################
+
+cv2.drawContours(frame_test, contours = possible_contours[0]['contour'], contourIdx=-1, color=(255, 0, 255), thickness = 5)
+plt.imshow(cv2.cvtColor(frame_test, cv2.COLOR_BGR2RGB))
 #%%
 x_fin = possible_contours[0]['cx']
 y_fin = possible_contours[0]['cy']
 print(x_fin)
 print(y_fin)
+
